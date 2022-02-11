bc7ed0020956d8b8cbaf387789045bb714a06a87
Jerkko Viisteensaari <jerkko.viisteensaari@tuni.fi>
2022-02-11 23:02:12 +0200

    {Exercise 3.19 finished}

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const errorHandler = (error, request, response, next) =>  {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
   }
 
   next(error)
@@ -57,21 +59,20 @@ const generateId = () => {
   return rndId
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  console.log(body)
-  //const redundantName = Person.find({}).then(persons => persons.name === body.name)
-  if (body === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
+    })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -83,16 +84,12 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
-      console.log('updated', updatedPerson)
       response.json(updatedPerson)
     })
     .catch(error => next(error))
