37bb5bd208db5e4b5630148354d2746b47742de3
vjerkko <vjerkko@gmail.com>
2022-02-18 13:05:43 +0200

    Finished exercise 3.19

--- models/person.js
+++ models/person.js
@@ -12,8 +12,14 @@ mongoose.connect(url)
 
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+  }
 })
 
 personSchema.set('toJSON', {
